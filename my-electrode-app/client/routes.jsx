f2aa2a7345a6d146b2ab32d406623c48300e292d
Danilo Zekovic <dzekovic@localhost.localdomain>
2017-02-28 10:33:33 +0100

    testing, playing with code

--- my-electrode-app/client/routes.jsx
+++ my-electrode-app/client/routes.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import {Route} from "react-router";
+import { Route, IndexRoute } from "react-router";
 import Home from "./components/home";
+import Foo from "./components/foo";
 
 export const routes = (
-  <Route path="/" component={Home}/>
+  <Route path="/" component={Home}>
+    <IndexRoute component={Home}/>
+    <Route path="/foo" component={Foo}/>
+  </Route>
+  //<Route path="/" component={Home}/>
 );
--- my-electrode-component/src/index.js
+++ my-electrode-component/src/index.js
@@ -1,1 +1,3 @@
-export { default as ElectrodeComponent } from "./components/electrode-component ";
+export { default as ElectrodeComponent } from "./components/electrode-component";
+export { default as RenderFriend } from "./components/render-friend";
+export { default as GuestList } from "./components/guest-list";
